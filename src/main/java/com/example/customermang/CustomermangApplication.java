024751b50419c43b9ac0590053d5ed24b518eb5e
Venkata Sai Bala Bhagyavathi <tirumalasetti.venkata-sai-bala-bhagyavathi@capgemini.com>
2024-01-11 17:56:19 +0530

    Initial commit

---
+++ src/main/java/com/example/customermang/CustomermangApplication.java
@@ -0,0 +1,13 @@
+package com.example.customermang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomermangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomermangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customermang/CustomermangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customermang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomermangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
